d098039f7c9b1639fed74c6f59414468146b0dbd
Stewart Bennell <stewart@bennell.me>
2024-06-07 13:31:19 +1000

    add Krita update script

---
+++ Intune/Proactive Remediations/Check and Update/KDE.Krita/Remediation.ps1
@@ -0,0 +1,21 @@
+﻿$app_2upgrade = "KDE.Krita"
+
+try{
+    # resolve and navigate to winget.exe
+    $Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
+
+    # upgrade command
+    &$winget upgrade --exact $app_2upgrade --silent --force --accept-package-agreements --accept-source-agreements
+    
+	## Remove Desktop Shortcuts
+    $DesktopSC = "$env:PUBLIC\desktop\Krita*.lnk"
+    If (Test-Path $DesktopSC) {
+    	Remove-Item $DesktopSC -Recurse -Force -ErrorAction SilentlyContinue
+    }
+	
+    exit 0
+
+}catch{
+    Write-Error "Error while installing upgrade for: $app_2upgrade"
+    exit 1\\
+}
