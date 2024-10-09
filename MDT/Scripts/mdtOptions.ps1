f0e7179719140444555f2becd663cad4c756a776
Stewart Bennell <stewart@bennell.me>
2024-10-10 09:40:01 +1100

    Update mdtOptions.ps1
    10.10.24-05:   Aligned HWID Activation and DONE  Version text.

--- MDT/Scripts/mdtOptions.ps1
+++ MDT/Scripts/mdtOptions.ps1
@@ -6,7 +6,7 @@
 # //
 # // File:      mdtOptions.ps1
 # // 
-# // Version:   09.10.24-04
+# // Version:   09.10.24-05
 # //
 # // Version History
 # // 25.01.24-01: Initial version
@@ -17,6 +17,7 @@
 # // 10.10.24-01:   Added HWID Activation Checkbox
 # // 10.10.24-03:   Added logging
 # // 10.10.24-04:   Added Windows Updates Checkbox
+# // 10.10.24-05:   Aligned HWID Activation and DONE  Version text.
 # // 
 # // Purpose:   Running Windows updates During Deployment.
 # // 
@@ -50,17 +51,9 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Form                            = New-Object system.Windows.Forms.Form
-$Form.ClientSize                 = New-Object System.Drawing.Point(341,250)  # Increased height to accommodate checkboxes
+$Form.ClientSize                 = New-Object System.Drawing.Point(341,200)  # Adjusted height for layout
 $Form.text                       = "Setting Options"
 $Form.TopMost                    = $false
-
-$Done                            = New-Object system.Windows.Forms.Button
-$Done.text                       = "DONE"
-$Done.width                      = 60
-$Done.height                     = 30
-$Done.visible                    = $true
-$Done.location                   = New-Object System.Drawing.Point(268,210)  # Updated location
-$Done.Font                       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $SetOrganisation                 = New-Object system.Windows.Forms.ComboBox
 $SetOrganisation.text            = "BENNELL IT"
@@ -82,14 +75,14 @@ $Organisation.Font               = New-Object System.Drawing.Font('Microsoft San
 $HWIDActivationCheckbox          = New-Object System.Windows.Forms.CheckBox
 $HWIDActivationCheckbox.text     = "HWID Activation"
 $HWIDActivationCheckbox.AutoSize  = $true
-$HWIDActivationCheckbox.location  = New-Object System.Drawing.Point(14,87)  # Updated location for the checkbox
+$HWIDActivationCheckbox.location  = New-Object System.Drawing.Point(14,87)  # Updated location for the HWID checkbox
 $HWIDActivationCheckbox.Font      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-# Adding Windows Updates Checkbox
+# Adding Windows Updates Checkbox next to HWID
 $WindowsUpdatesCheckbox          = New-Object System.Windows.Forms.CheckBox
 $WindowsUpdatesCheckbox.text     = "Windows Updates"
 $WindowsUpdatesCheckbox.AutoSize  = $true
-$WindowsUpdatesCheckbox.location  = New-Object System.Drawing.Point(14,110)  # Location for the Windows Updates checkbox
+$WindowsUpdatesCheckbox.location  = New-Object System.Drawing.Point(150,87)  # Moved next to the HWID checkbox
 $WindowsUpdatesCheckbox.Font      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 $WindowsUpdatesCheckbox.Checked   = $true  # Set default to checked
 
@@ -98,14 +91,14 @@ $Variable.text                   = "Variable"
 $Variable.AutoSize               = $true
 $Variable.width                  = 25
 $Variable.height                 = 10
-$Variable.location               = New-Object System.Drawing.Point(14,140)
+$Variable.location               = New-Object System.Drawing.Point(14,120)  # Adjusted position
 $Variable.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $SetVariable                     = New-Object System.Windows.Forms.TextBox
 $SetVariable.multiline           = $false
 $SetVariable.width               = 226
 $SetVariable.height              = 20
-$SetVariable.location            = New-Object System.Drawing.Point(104,140)
+$SetVariable.location            = New-Object System.Drawing.Point(104,120)
 $SetVariable.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $Header                          = New-Object system.Windows.Forms.Label
@@ -118,14 +111,23 @@ $Header.Font                     = New-Object System.Drawing.Font('Microsoft San
 
 # Updated Version label
 $Version                         = New-Object system.Windows.Forms.Label
-$Version.text                    = "Version:09.10.24-04"  # Updated version here
+$Version.text                    = "Version:09.10.24-05"  # Updated version here
 $Version.AutoSize                = $true
 $Version.width                   = 25
 $Version.height                  = 10
-$Version.location                = New-Object System.Drawing.Point(15,180)
+$Version.location                = New-Object System.Drawing.Point(15,160)  # Adjusted position for version
 $Version.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$Form.controls.AddRange(@($Done,$SetOrganisation,$Organisation,$HWIDActivationCheckbox,$WindowsUpdatesCheckbox,$Variable,$SetVariable,$Header,$Version))
+# Align DONE button next to Version text on the right
+$Done                            = New-Object system.Windows.Forms.Button
+$Done.text                       = "DONE"
+$Done.width                      = 60
+$Done.height                     = 30
+$Done.visible                    = $true
+$Done.location                   = New-Object System.Drawing.Point(250,155)  # Updated location next to Version
+$Done.Font                       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$Form.controls.AddRange(@($SetOrganisation,$Organisation,$HWIDActivationCheckbox,$WindowsUpdatesCheckbox,$Variable,$SetVariable,$Header,$Version,$Done))
 
 $Done.Add_Click({ Set-Settings })
 
