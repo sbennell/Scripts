f8065213c100d18ba114c744739e595032d2945c
Stewart Bennell <stewart@bennell.me>
2024-10-10 09:28:57 +1100

    Update mdtOptions.ps1
    10.10.24-04:   Added Windows Updates Checkbox

--- MDT/Scripts/mdtOptions.ps1
+++ MDT/Scripts/mdtOptions.ps1
@@ -6,7 +6,7 @@
 # //
 # // File:      mdtOptions.ps1
 # // 
-# // Version:   09.10.24-03
+# // Version:   09.10.24-04
 # //
 # // Version History
 # // 25.01.24-01: Initial version
@@ -16,6 +16,7 @@
 # // 21.12.22-01:   Remove Set Recovery
 # // 10.10.24-01:   Added HWID Activation Checkbox
 # // 10.10.24-03:   Added logging
+# // 10.10.24-04:   Added Windows Updates Checkbox
 # // 
 # // Purpose:   Running Windows updates During Deployment.
 # // 
@@ -39,7 +40,7 @@ function Log-Message {
     )
     $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
     Add-Content -Path "$logFile" -Value "$timestamp - $message"
-
+}
 
 # Temporarily close the TS progress UI
 $TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
@@ -49,7 +50,7 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Form                            = New-Object system.Windows.Forms.Form
-$Form.ClientSize                 = New-Object System.Drawing.Point(341,230)  # Increased height to accommodate checkbox
+$Form.ClientSize                 = New-Object System.Drawing.Point(341,250)  # Increased height to accommodate checkboxes
 $Form.text                       = "Setting Options"
 $Form.TopMost                    = $false
 
@@ -58,7 +59,7 @@ $Done.text                       = "DONE"
 $Done.width                      = 60
 $Done.height                     = 30
 $Done.visible                    = $true
-$Done.location                   = New-Object System.Drawing.Point(268,180)  # Updated location
+$Done.location                   = New-Object System.Drawing.Point(268,210)  # Updated location
 $Done.Font                       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $SetOrganisation                 = New-Object system.Windows.Forms.ComboBox
@@ -77,26 +78,34 @@ $Organisation.height             = 10
 $Organisation.location           = New-Object System.Drawing.Point(14,50)
 $Organisation.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-# Adding HWID Activation Checkbox (moved to where Recovery was)
+# Adding HWID Activation Checkbox
 $HWIDActivationCheckbox          = New-Object System.Windows.Forms.CheckBox
 $HWIDActivationCheckbox.text     = "HWID Activation"
 $HWIDActivationCheckbox.AutoSize  = $true
 $HWIDActivationCheckbox.location  = New-Object System.Drawing.Point(14,87)  # Updated location for the checkbox
 $HWIDActivationCheckbox.Font      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
+# Adding Windows Updates Checkbox
+$WindowsUpdatesCheckbox          = New-Object System.Windows.Forms.CheckBox
+$WindowsUpdatesCheckbox.text     = "Windows Updates"
+$WindowsUpdatesCheckbox.AutoSize  = $true
+$WindowsUpdatesCheckbox.location  = New-Object System.Drawing.Point(14,110)  # Location for the Windows Updates checkbox
+$WindowsUpdatesCheckbox.Font      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$WindowsUpdatesCheckbox.Checked   = $true  # Set default to checked
+
 $Variable                        = New-Object system.Windows.Forms.Label
 $Variable.text                   = "Variable"
 $Variable.AutoSize               = $true
 $Variable.width                  = 25
 $Variable.height                 = 10
-$Variable.location               = New-Object System.Drawing.Point(14,120)
+$Variable.location               = New-Object System.Drawing.Point(14,140)
 $Variable.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$SetVariable                     = New-Object system.Windows.Forms.TextBox
+$SetVariable                     = New-Object System.Windows.Forms.TextBox
 $SetVariable.multiline           = $false
 $SetVariable.width               = 226
 $SetVariable.height              = 20
-$SetVariable.location            = New-Object System.Drawing.Point(104,120)
+$SetVariable.location            = New-Object System.Drawing.Point(104,140)
 $SetVariable.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $Header                          = New-Object system.Windows.Forms.Label
@@ -109,21 +118,20 @@ $Header.Font                     = New-Object System.Drawing.Font('Microsoft San
 
 # Updated Version label
 $Version                         = New-Object system.Windows.Forms.Label
-$Version.text                    = "Version:10.10.24-03"  # Updated version here
+$Version.text                    = "Version:09.10.24-04"  # Updated version here
 $Version.AutoSize                = $true
 $Version.width                   = 25
 $Version.height                  = 10
-$Version.location                = New-Object System.Drawing.Point(15,160)
+$Version.location                = New-Object System.Drawing.Point(15,180)
 $Version.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$Form.controls.AddRange(@($Done,$SetOrganisation,$Organisation,$HWIDActivationCheckbox,$Variable,$SetVariable,$Header,$Version))
+$Form.controls.AddRange(@($Done,$SetOrganisation,$Organisation,$HWIDActivationCheckbox,$WindowsUpdatesCheckbox,$Variable,$SetVariable,$Header,$Version))
 
 $Done.Add_Click({ Set-Settings })
 
 Function Set-Settings 
 {	
 	# Set organisation and log it
-	#$TSEnv = New-Object -COMObject Microsoft.SMS.TSEnvironment
 	$organisation = $SetOrganisation.Text.ToUpper()
 	$TSEnv.Value("organisation") = "$($organisation)"
 	Log-Message "Organisation set to: $organisation"
@@ -137,6 +145,11 @@ Function Set-Settings
 	$HWIDActivation = $HWIDActivationCheckbox.Checked
 	$TSEnv.Value("HWIDActivation") = "$($HWIDActivation)"
 	Log-Message "HWID Activation set to: $HWIDActivation"
+	
+	# Set Windows Updates based on the checkbox state and log it
+	$WindowsUpdates = $WindowsUpdatesCheckbox.Checked
+	$TSEnv.Value("WindowsUpdates") = "$($WindowsUpdates)"
+	Log-Message "Windows Updates set to: $WindowsUpdates"
 	
 	# Log done button clicked
 	Log-Message "Settings completed. Closing the form."
